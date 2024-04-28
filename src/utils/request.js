20762f48dbb26ff43f0b0c0f5d604cdd9fc8fc6e
ahmad <ahmadmuthna4@gmail.com>
2024-04-28 14:35:24 +0300

    deploy

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const request = axios.create({
-    baseURL: "http://localhost:5000"
-    // baseURL: "https://rmss.onrender.com"
+    // baseURL: "http://localhost:5000"
+    baseURL: "https://rmss.onrender.com"
 });
 
 export default request;
